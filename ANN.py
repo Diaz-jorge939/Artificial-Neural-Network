1f0c92328033cdb56ed2b7acc378285a217f4104
Diaz-jorge939 <90347792+Diaz-jorge939@users.noreply.github.com>
2023-03-20 01:02:21 -0700

    Neural Network construction part 1

--- ANN.py
+++ ANN.py
@@ -3,17 +3,17 @@ import os
 
 class Node():
     def __init__(self):
-        self.collector = None
+        self.collector = 0
         self.connections = []
 
 class ANN():
     def __init__(self, input_layer, hidden_layer, output_layer):
         
-        self.input_layer = [Node() for index in range(input_layer)]         # list comprehension adding a Node() at each index of hidden_layer list 
+        self.input_layer = [Node() for index in range(input_layer)]      # list comprehension adding a Node() at each index of hidden_layer list 
         self.hidden_layer = [Node() for index in range(hidden_layer)]
         self.output_layer = [Node() for index in range(output_layer)]
 
-        self.network = [self.input_layer, self.hidden_layer, self.output_layer]
+        self.network = [self.input_layer, self.hidden_layer, self.output_layer]     # network contains 3 lists at each index 
 
     def add_inputs(self, input_vals):
         if len(input_vals) == len(self.input_layer):   
@@ -24,16 +24,42 @@ class ANN():
         else: 
             print("invald size")
 
-    def add_connections(self):
-        pass
+    def add_connections(self,layer1, layer2):
+        for i in range(len(layer1)):
+            for j in range(len(layer2)):     
+                layer1[i].connections.append(layer2[j])      
+                
+    def sum_collector(self):
+        # for layer_index in range(len(self.network)-1):             # iterating through each layer
+        #     for node in self.network[layer_index]:              # for each node in layer
+        #         for i in range(len(node.connections)):
+        #             node.connections[i].collector = node.connections[i].collector + node.collector
+        for layer in self.network:
+            for node in layer:
+                for connected_node in node.connections:
+                    connected_node.collector = connected_node.collector + node.collector
 
- 
-with open('input.txt', 'r') as read_file:
-    input_file = read_file.read()
+if __name__ == '__main__':
+    with open('Assignment 2/input.txt', 'r') as read_file:
+        input_file = read_file.read()
 
-inputs = input_file.split(',')
+    inputs = input_file.split(',')
+    
+    # print(input_file)
+    # print(inputs)
+    
+    new_inputs = [float(val) for val in inputs]
 
-print(input_file)
-print(inputs)
+    net = ANN(4,2,1)
 
-net = ANN(4,2,1)
+    net.add_inputs(new_inputs)
+
+    net.add_connections(net.input_layer, net.hidden_layer)
+    net.add_connections(net.hidden_layer,net.output_layer)
+
+
+    net.sum_collector()
+
+    for layer in net.network:
+        for node in layer:
+            print(node.collector)
